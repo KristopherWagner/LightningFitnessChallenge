f89e2d1de08716626d0b024c8297e885d4d4a7d9
KristopherWagner <kmwagner91@gmail.com>
2022-08-17 13:16:35 -0400

    Use 2 spaces not 4

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,15 @@ import './App.css';
 import { LoginButton } from './components';
 
 export default function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
           This page is a work in progress
-                </p>
-                <LoginButton />
-            </header>
-        </div>
-    );
+        </p>
+        <LoginButton />
+      </header>
+    </div>
+  );
 }
--- client/src/pages/Token.js
+++ client/src/pages/Token.js
@@ -3,32 +3,32 @@ import axios from 'axios';
 import { useSearchParams } from 'react-router-dom';
 
 async function getTokens({ code }) {
-    const response = await axios.post('https://www.strava.com/oauth/token', {
-        client_id: 45959,
-        client_secret: '',
-        code,
-        grant_type: 'authorization_code',
-    });
+  const response = await axios.post('https://www.strava.com/oauth/token', {
+    client_id: 45959,
+    client_secret: '',
+    code,
+    grant_type: 'authorization_code',
+  });
 
-    return response.data;
+  return response.data;
 }
 
 export default function Token() {
-    const [params, ] = useSearchParams();
-    const [token, setToken] = useState(null);
-    const code = params.get('code');
+  const [params, ] = useSearchParams();
+  const [token, setToken] = useState(null);
+  const code = params.get('code');
 
-    useEffect(() => {
-        const loadTokens = async () => {
-            try {
-                const response = await getTokens({ code });
-                console.log(response);
-                setToken(response);
-            } catch(error) {
-                console.error(error);
-            }
-        };
-        loadTokens();
-    }, [code]);
-    return <p>{JSON.stringify(token, 0, 2)}</p>;
+  useEffect(() => {
+    const loadTokens = async () => {
+      try {
+        const response = await getTokens({ code });
+        console.log(response);
+        setToken(response);
+      } catch(error) {
+        console.error(error);
+      }
+    };
+    loadTokens();
+  }, [code]);
+  return <p>{JSON.stringify(token, 0, 2)}</p>;
 }
