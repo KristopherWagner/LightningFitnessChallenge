087fb2abe27b1c54ffa177974b3e1b7add0e01af
KristopherWagner <kmwagner91@gmail.com>
2022-08-18 13:28:23 -0400

    #3 Ask for activity read

--- client/src/components/LoginButton/LoginButton.js
+++ client/src/components/LoginButton/LoginButton.js
@@ -1,7 +1,7 @@
 export default function LoginButton() {
   const client_id = 45959;
   const redirect_uri = 'http://localhost:3000/';
-  const url = `http://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}exchange_token&approval_prompt=force&scope=read`;
+  const url = `http://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}exchange_token&approval_prompt=force&scope=read,activity:read`;
 
   return (
     <a href={url}>
