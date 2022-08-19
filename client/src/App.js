3230282d3054008c8240bd1af1fb38d06e0febae
KristopherWagner <kmwagner91@gmail.com>
2022-08-19 15:06:25 -0400

    Add a header

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-import { LoginButton } from './components';
+import { Header, LoginButton } from './components';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This page is a work in progress
-        </p>
-        <LoginButton />
-      </header>
+      <Header />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        This page is a work in progress
+      </p>
+      <LoginButton />
     </div>
   );
 }
